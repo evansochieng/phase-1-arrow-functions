cd4384ffe2940817b16b46aede7cef46fd3be33d
Evans Ochieng <evansochieng121@gmail.com>
2022-08-13 16:50:02 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+// Function expression
+// Divide function 
+const divide = function(){
+    return (2000 / 100);
+};
+
+// Arrow function
+// Square function
+const square = number => number * number;
+
+// Add function
+const add = (number1, number2) => (number1 + number2);
